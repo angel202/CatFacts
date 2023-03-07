b957abbbeed5f4aebe7f08160ee0bb3c999e8074
angel202 <angel.g5170@gmail.com>
2023-03-07 12:07:30 -0600

    Pulling data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
-import react from "react";
 import axios from "axios"; 
+import {useState, useEffect} from 'react'; 
 
 function App() {
+
+  const [catData, setCatData] = useState(""); 
+
+  useEffect(() => {
+    axios.get("https://catfact.ninja/fact").then((response) =>{
+      setCatData([response.data]);
+      console.log(response.data)
+    });
+  }, []);  
+ 
+  /*
+  const fetchData = async () => { 
+    try { 
+      const data = await axios.get("https://cat-fact.herokuapp.com/facts");
+      console.log(data); 
+      setCatData(data);
+    } catch (err){ 
+      console.log(err); 
+    }
+  }; */
+
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+
+      {catData && catData.map((val) => {
+        return(
+          <li key={val}>{val.fact}</li>
+        );
+      })};
+
+
     </div>
   );
 }
