7d86350986811f880202cf598ad785f18005e828
angel202 <angel.g5170@gmail.com>
2023-03-21 00:25:27 -0500

    Styling/cleaning code

--- client/src/CData.js
+++ client/src/CData.js
@@ -4,14 +4,18 @@ const CData = ({catData}) => {
     return (
         <div class = "card-deck">
                   <div class="card-body">
-                      <div class="card">
-                          <h5 class="card-title">The cat's breed is {catData.breed}</h5>
-                          <h6 class="card-text"> Some Facts about the cat: </h6>
+                    <div class="d-inline-flex">
+                      <div class="card" style={{backgroundColor:'black'}}>
+                          <h3 class="card-title">The cat's breed is {catData.breed}</h3>
+                          <h4 class="card-text"> Some Facts about the cat: </h4>
+                          <ul>
                           <li> It's country of origin is: {catData.country}</li>
                           <li> Their hair coat is: {catData.coat}.</li>
                           <li> It's pattern is: {catData.pattern}.</li>
+                          </ul>
                           <p/>
                       </div>
+                    </div> 
                   </div>
         </div>
 
